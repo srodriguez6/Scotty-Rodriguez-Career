0cae175d5ba3db9237ee066a95da07e591f51622
srodriguez6 <47450775+srodriguez6@users.noreply.github.com>
2019-11-06 16:31:59 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 Copysrc/pages/index.js: copy code to clipboard
 import React from "react"
 export default () => (
-  <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+  <div style={{ color: 'purple', fontSize: '72px' }}>Hello Gatsby!</div>
 )
